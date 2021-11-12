e9dce6c604d17c33bb09aa52855c7f69b440aa75
A. Stewart <iamalexstewart@gmail.com>
2021-11-11 21:12:21 -0500

    Consolidate sub-resources under their parent
    `Buyer` and `Refund` are sub-resources of `Invoice`, thus it is pragmatic to implement and test them within their parent's context.

--- tests/LaravelBitpayInvoiceTest.php
+++ tests/LaravelBitpayInvoiceTest.php
@@ -2,7 +2,9 @@
 
 namespace Vrajroham\LaravelBitpay\Tests;
 
+use BitPaySDK\Model\Invoice\Buyer;
 use BitPaySDK\Model\Invoice\Invoice as BitPaySDKInvoice;
+use BitPaySDK\Model\Invoice\Refund;
 use PHPUnit\Framework\TestCase;
 use Vrajroham\LaravelBitpay\LaravelBitpay;
 
@@ -12,5 +14,17 @@ class LaravelBitpayInvoiceTest extends TestCase
     public function isInstanceOfInvoice()
     {
         $this->assertEquals(LaravelBitpay::Invoice() instanceof BitPaySDKInvoice, true);
+    }
+
+    /** @test */
+    public function isInstanceOfBuyer()
+    {
+        $this->assertEquals(LaravelBitpay::Buyer() instanceof Buyer, true);
+    }
+
+    /** @test */
+    public function isInstanceOfRefund()
+    {
+        $this->assertEquals(LaravelBitpay::Refund() instanceof Refund, true);
     }
 }
