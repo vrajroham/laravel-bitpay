4055d6f03cce267d7897332e11c663c2f4d95976
A. Stewart <iamalexstewart@gmail.com>
2021-11-11 21:23:51 -0500

    Subscriptions resource implementation

--- src/LaravelBitpay.php
+++ src/LaravelBitpay.php
@@ -4,28 +4,25 @@ namespace Vrajroham\LaravelBitpay;
 
 use BitPaySDK\Exceptions\BitPayException;
 use Vrajroham\LaravelBitpay\Actions\ManageBills;
-use Vrajroham\LaravelBitpay\Actions\ManageBuyers;
 use Vrajroham\LaravelBitpay\Actions\ManageCurrencies;
+use Vrajroham\LaravelBitpay\Actions\ManageExchangeRates;
 use Vrajroham\LaravelBitpay\Actions\ManageInvoices;
-use Vrajroham\LaravelBitpay\Actions\ManageItems;
-use Vrajroham\LaravelBitpay\Actions\ManageRefunds;
-use Vrajroham\LaravelBitpay\Tests\ManageExchangeRates;
+use Vrajroham\LaravelBitpay\Actions\ManageSubscriptions;
 use Vrajroham\LaravelBitpay\Traits\MakesHttpRequests;
 
 
 class LaravelBitpay
 {
     use MakesHttpRequests;
-    use ManageInvoices;
-    use ManageBuyers;
-    use ManageItems;
     use ManageBills;
     use ManageCurrencies;
     use ManageExchangeRates;
-    use ManageRefunds;
+    use ManageInvoices;
+    use ManageSubscriptions;
+
 
     protected $client;
-    private $config;
+    private   $config;
 
     /**
      * Setup client while creating the instance.
